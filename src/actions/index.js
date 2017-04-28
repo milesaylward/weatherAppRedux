4d426117d68b5cfb555a971f04bc79f33d35c78d
Miles Aylward <miles.aylward@gmail.com>
2017-04-28 09:59:20 -0400

    add redux app

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import Axios from 'axios'
+
+const API_KEY = 'dbd50e9ade842ad9cacca50aef829779';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = Axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
